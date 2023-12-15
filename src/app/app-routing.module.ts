dc33ad02f79131cfa87db2699254513f257c4d3a
Abdularhman Ahmed <abdwbedoo@gmail.com>
2023-12-15 11:05:44 +0200

    Create signup component initialize lazyrouting for standalone components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:'', pathMatch:'full', loadComponent: () => import('./components/core/home/home.component').then ( (c) => c.HomeComponent ) },
+  {path:'signup', loadComponent: () => import('./components/core/reactive-forms/signup/signup.component').then ( (c) => c.SignupComponent ) }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
