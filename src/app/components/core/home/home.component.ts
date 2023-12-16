6088a85803d482997e7ca3ce96306599eaf4ed12
Abdularhman Ahmed <abdwbedoo@gmail.com>
2023-12-17 01:03:57 +0200

    update Home page

--- src/app/components/core/home/home.component.ts
+++ src/app/components/core/home/home.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HoverDirective } from 'src/app/directives/hovering/hover.directive';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HoverDirective],
+  hostDirectives:[
+    {
+      directive: HoverDirective
+    }
+  ],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
